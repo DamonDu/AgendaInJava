6e39bc28e25722c6c6f155a11c19de8c200bd2eb
DamonDu <sy20030260@163.com>
2017-10-10 06:34:48 +0800

    program entry

--- src/com/me/AgendaService.java
+++ src/com/me/AgendaService.java
@@ -1,24 +1,29 @@
 package com.me;
 
 import com.me.cmd.Cmd;
-import com.me.data.User;
+import com.me.ui.UI;
 import com.me.util.Processor;
 
 import java.util.Scanner;
 
-import static java.util.Objects.hash;
-
+/**
+ * <h1>AgendaService</h1>
+ * <p>程序入口，main函数调用一个 operationLoop 函数，提示用户输入命令，读取命令并调用函数处理。
+ *
+ */
 public class AgendaService {
 
     public static void main(String[] args) {
-	// write your code here
+        UI.printHelp();
         operationLoop();
     }
 
     static void operationLoop() {
         while (true) {
+            UI.printSymbol();
             Scanner scanner = new Scanner(System.in);
-            String str = scanner.next();
+            String str = scanner.nextLine();
+            //System.out.println(str);
             if(Processor.process(new Cmd(str)) == 1) {
                 break;
             }
